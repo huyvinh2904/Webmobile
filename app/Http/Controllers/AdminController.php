64ee54446c61620744a07906dff45fdbea76f805
huyvinh2904 <tranvinhhuy2904@gmail.com>
2017-04-01 00:31:55 +0700

    Category and Item

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,22 +15,32 @@ class AdminController extends Controller
     }
     public function postLogin(Request $request){
 
-    	if (Auth::attempt(['email'=>$request->email,'password'=>$request->password])) {
-			echo "abcxyz";
-		}
-		else{
-			return redirect('admin.login')->with('thongbao','Dang nhap khong thanh cong');
-		}
+    	$this->validate($request,
+            [
+            'email'=>'required|min:5|max:40',
+            'password'=>'required'
 
+
+            ],
+            [
+            'email.required'=>'Ban chua nhap email...',
+            'email.min'=>'it nhat 5 ky tu',
+            'email.max'=>'Nhieu nhat 40 ky tu',
+            'password.required'=>'Ban chua nhap mat khau'
+
+            ]);
+        if (Auth::attempt(['email'=>$request->email,'password'=>$request->password,'level'=>1])) {
+            return redirect('item/list');
+        }
+        else{
+            return redirect('login')->with('thongbao','Dang nhap khong thanh cong');
+        }
 
     }
     public function getLogout(){
         Auth::logout();
 
-        return redirect('admin.login');
-
-        
-
+        return redirect('login');
     }
     }
 
--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -1,15 +1,16 @@
-<!-- /.navbar-top-links -->
-@extends('admin.layout.index')
-@section('content')
-     <div id="page-wrapper">
+ @extends('admin.layout.index')
+  @section('content')
+
+<div id="page-wrapper">
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">Category
-                            <small>Add</small>
+                            <small>Edit</small>
                         </h1>
                     </div>
                     <!-- /.col-lg-12 -->
+
                     <div class="col-lg-7" style="padding-bottom:120px">
                     @if(count($errors)>0)
                     <div class="alert alert-danger">
@@ -26,24 +27,29 @@
                     </div>
 
                     @endif
-                        <form action="cate/add" method="POST">
+                        <form action="cate/edit/{{$CateEdit->id}}" method="POST">
                         <input type="hidden" name="_token" value="{{csrf_token()}}">
                             <div class="form-group">
-                                <label>Thể Loại</label>
-                                <select class="form-control" name="theloai">
-                                  @foreach($Item as $tl)
-                                    <option value="{{$tl->id}}">{{$tl->name}}</option>
-                                    
+                                <label>Menu</label>
+                                <select class="form-control" name="theloai1">
+                                    <option value="0">Please Choose Category</option>
+                                    @foreach($Item as $tl)
+                                    <option
+                                    @if($CateEdit->item == $tl->id)
+                                       {{"selected"}}
+                                    @endif
+                                     value="{{$tl->id}}">{{$tl->name}}</option>
                                     @endforeach
-                                
+                                    
+                                    
                                 </select>
                             </div>
                             <div class="form-group">
                                 <label>Category</label>
-                                <input class="form-control" name="txtCateName" placeholder="Please Enter Category Name" />
+                                <input class="form-control" name="txtCateName" placeholder="Please Enter Category..." value="{{$CateEdit->name}}" />
                             </div>
                             
-                            <button type="submit" class="btn btn-default">Category Add</button>
+                            <button type="submit" class="btn btn-default"> Sửa</button>
                             <button type="reset" class="btn btn-default">Reset</button>
                         <form>
                     </div>
@@ -52,4 +58,4 @@
             </div>
             <!-- /.container-fluid -->
         </div>
-@endsection
+        @endsection
